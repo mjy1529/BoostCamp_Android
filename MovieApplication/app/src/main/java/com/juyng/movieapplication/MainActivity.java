67fc5ac362b20cd62aa809de9d64057c10113f61
jooyeong <young951109@naver.com>
2018-12-27 01:43:44 +0900

    데이터바인딩하여 버튼 클릭 이벤트 실행

--- MovieApplication/app/src/main/java/com/juyng/movieapplication/MainActivity.java
+++ MovieApplication/app/src/main/java/com/juyng/movieapplication/MainActivity.java
@@ -1,13 +1,25 @@
 package com.juyng.movieapplication;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.juyng.movieapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //데이터 바인딩
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setActivity(this);
+    }
+    
+    public void onButtonClick(View view) {
+        Toast.makeText(this, "clicked!", Toast.LENGTH_SHORT).show();
     }
 }
